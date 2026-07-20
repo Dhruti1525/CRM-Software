036cd6d90f46dcd8a1930c96352d9f5660583ef5
Dhruti1525 <dhrutiaksh1542@gmail.com>
2026-07-20 23:03:10 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/CRM-Software/",
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 241a0a9c4598046ba167cbf4df560db04a01562d
